7416a8a07dfd52cead1fa1b2a89936e21c15b8a3
qiisziilbash <m.shafiei0423@gmail.com>
2020-02-26 10:06:51 -0700

    main.py is documented

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ from bs4 import BeautifulSoup as bs
 
 
 def crawl_youtube_channel(channel_url):
+    """ gets a Youtube channel url and returns a dictionary containing info about the videos"""
+
     videos_url = channel_url + '/videos'
 
+    # if you get error look at readme file for instructions
     driver = webdriver.Firefox()
     driver.get(videos_url)
 
     time.sleep(3)
 
     # scroll dow to the button of the page
+    print("Opening the channel in FireFox and scrolling to the bottom of the page ....")
     while True:
         old_height = driver.execute_script("return document.documentElement.scrollHeight")
         driver.execute_script("window.scrollTo(0, " + str(old_height) + ");")
@@ -22,6 +26,7 @@ def crawl_youtube_channel(channel_url):
         if new_height == old_height:
             break
 
+    # parse the html and get the links for the videos
     soup = bs(driver.page_source, "html.parser")
     video_tags = soup.findAll('a', attrs={'class': 'yt-simple-endpoint style-scope ytd-grid-video-renderer'})
     links = []
@@ -33,6 +38,6 @@ def crawl_youtube_channel(channel_url):
 
 
 if __name__ == '__main__':
-    # provide the youtube channel url
+    # provide the youtube channel url here
     youtube_url = 'https://www.youtube.com/user/jadi19jadi19'
     crawl_youtube_channel(youtube_url)
